8dee260fda1d462f00e866a5e4d3401d9f3b9cec
Nduoma132 <nduoma132@gmail.com>
2024-09-19 23:36:35 +0100

    Seventh commit

--- app.js
+++ app.js
@@ -24,11 +24,7 @@
 
 const hamburger = document.querySelector('.hamburger');
 const navigation = document.querySelector('.mobile-navigation');
-
 hamburger.addEventListener('click', () => {
-  // Toggle the "close" class to switch between hamburger and "X" button
   hamburger.classList.toggle('close');
-  
-  // Toggle the visibility of the mobile navigation menu
   navigation.classList.toggle('active');
 });
--- dashboard.js
+++ dashboard.js
@@ -2,9 +2,6 @@ const hamburger = document.querySelector('.hamburger');
 const navigation = document.querySelector('.mobile-navigation');
 
 hamburger.addEventListener('click', () => {
-  // Toggle the "close" class to switch between hamburger and "X" button
   hamburger.classList.toggle('close');
-  
-  // Toggle the visibility of the mobile navigation menu
   navigation.classList.toggle('active');
 });
